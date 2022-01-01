223cfa95975d314e004b723ed96dd24314ed11fd
LalitKumar97 <lalitkumar9710888@gmail.com>
2022-01-01 13:20:38 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function display(val)
+{
+ document.getElementById("screen").value+=val;
+}
+function solve()
+{
+    let x = document.getElementById("screen").value;
+    let y = eval(x);
+    document.getElementById("screen").value = y;
+}
+function clr()
+{
+  document.getElementById("screen").value = " ";
+}
+function del()
+{
+    document.getElementById("screen").value = " ";
+}
